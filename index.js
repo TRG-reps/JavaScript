a067dbac5c90fc96097472c50d52cf6be2403007
Ruslan <tomash.ocenka@gmail.com>
2026-07-03 22:22:03 +0300

    arreys

--- index.js
+++ index.js
@@ -1,182 +1,166 @@
-// Задание 1: "Карточка пользователя"
-// Создай объект `user`, у которого будут свойства:
-// - `name` (строка)
-// - `age` (число)
-// - `isAdmin` (булевое значение)
-// Выведи в консоль:
-// - имя пользователя
-// - его возраст
-// - является ли он администратором
-
-const user = {
-  name: "Anna",
-  age: 20,
-  isAdmin: true
-}
-
-console.log (user.name, user.age, user.isAdmin) 
-
-// Задание 2: "Добавление и изменение"
-// 1. Создай объект `car` с маркой и годом выпуска.
-// 2. Добавь к нему свойство `color`.
-// 3. Измени год выпуска.
-// 4. Выведи результат в консоль.
-
-const car = {
-  brand: "Toyota",
-  year: 2024
-}
-
-car.color = "black";
-car.year = 2026
-
-console.log (car)
-
-// Задание 3: "Вложенные объекты"
-// Создай объект `order`, у которого:
-// - есть вложенный объект `customer` с полями `name` и `phone`
-// - есть вложенный объект `shipping` с полями `city` и `zip`
-// Выведи в консоль поля customer и shipping:
-
-const order = { 
-  customer : {
-    name: "Anna",
-    phone: 7777
-  },
-  shipping : {
-    city: "London",
-    zip: 5222
-  }
-}
-
-console.log (order.customer, order.shipping)
-
-// Задание 4: "Объект как база данных"
-// 1. Создай объект `products`, где ключами будут названия товаров, а значениями — цены.
-// 2. Получи цену конкретного товара по ключу.
-// 3. Добавь новый товар и выведи обновлённый объект.
-
+// Задание 1: "Добавление элемента"
+// Создай массив `fruits`
+// Создай функцию `addFruit(arr, fruit)`:
+// - добавляет fruit в конец массива
+// - возвращает обновлённый массив
+// Сохрани результат вызова функции в переменную и выведи её в консоль
 // Пример:
-// В консоли - "Товар: book, цена - 500"
-// В консоли - "Товар: pensil, цена - 50" 
+// const result = addFruit(fruits, "apple")
 
+const fruits = [];
 
-const products = {
-book: 500,
-pencil: 50,
-pen: 100,
-workbook: 300
+function addFruit(arr, fruit) {
+  arr.push(fruit);
+  return arr;
 }
 
-console.log (`Товар: book, цена -  ${products.book}`)
-console.log (`Товар: pencil, цена -  ${products.pencil}`)
+const result = addFruit(fruits, "apple");
+console.log(result);
 
+// Задание 2: "Удаление последнего"
+// Создай массив `numbers`
+// Создай функцию `removeLast(arr)`:
+// - удаляет последний элемент
+// - возвращает удалённое значение
+// Сохрани результат в переменную и выведи в консоль
+// Пример:
+// const removed = removeLast(numbers)
 
-// Задание 5 "Реальный заказ"
-// Создай объект `fullOrder`, в котором:
-// - `buyer`: содержет поля - name, email, phone
-// - `shipping`: содержет поля - city, adres, shipMethod
-// - `payment`: содержет поля - paymentMethod, status
-// Выведи в консоль объект, после чего измени значение каждого поля и снова выведи в консоль.
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeLast(arr) {
+  return arr.pop();
+}
 
-const fullOrder = {
-  buyer : {
-    name: "Kolya",
-    email: "kolya@gmail.com",
-    phone: "7777-8888"
-  },
-  shipping : {
-    city: "Oslo",
-    adress: "Highway 65",
-    shipMethod: "airplane"
-  },
-  payment : {
-    paymentMethod: "credit card",
-    status: "inProgress"
+const removed = removeLast(numbers);
+console.log(numbers);
+
+// Задание 3: "Проверка наличия"
+// Создай массив `users`
+// Создай функцию `hasUser(arr, name)`:
+// - проверяет, есть ли name в массиве
+// - возвращает true или false
+// Сохрани результат в переменную и выведи в консоль
+// Пример:
+// const exists = hasUser(users, "Alex")
+
+const users = [];
+
+function hasUser(arr, name) {
+  return arr.includes(name);
+}
+
+const exists = hasUser(users, "Alex");
+console.log(exists);
+
+// Задание 4: "Получить индекс"
+// Создай массив `colors`
+// Создай функцию `getIndex(arr, color)`:
+// - возвращает индекс элемента
+// - если нет — возвращает -1
+// Сохрани результат в переменную и выведи в консоль
+// Пример:
+// const index = getIndex(colors, "red")
+
+const colors = ["red"];
+
+function getIndex(arr, color) {
+  return arr.indexOf(color);
+}
+
+const index = getIndex(colors, "red");
+console.log(index);
+
+// Задание 5: "Добавление в начало"
+// Создай массив `tasks`
+// Создай функцию `addTaskToStart(arr, task)`:
+// - добавляет задачу в начало массива
+// - возвращает массив
+// Сохрани результат в переменную и выведи в консоль
+// Пример:
+// const updatedTasks = addTaskToStart(tasks, "Сделать ДЗ")
+
+const tasks = [];
+
+function addTaskToStart(arr, task) {
+  arr.unshift(task);
+  return arr;
+}
+
+const updatedTasks = addTaskToStart(tasks, "Сделать ДЗ");
+console.log(updatedTasks);
+
+// Задание 6: "Удаление первого элемента"
+// Создай массив `messages`
+// Создай функцию `removeFirst(arr)`:
+// - удаляет первый элемент массива
+// - возвращает его
+// Сохрани результат в переменную и выведи в консоль
+// Пример:
+// const first = removeFirst(messages)
+
+const messages = ["Hi", "Hello"];
+
+function removeFirst(arr) {
+  return arr.shift();
+}
+
+const first = removeFirst(messages);
+console.log(first);
+
+// Задание 7: "Преобразование в строку"
+// Создай массив `words`
+// Создай функцию `makeSentence(arr)`:
+// - объединяет элементы массива в строку через пробел
+// - возвращает строку
+// Сохрани результат в переменную и выведи в консоль
+// Пример:
+// const sentence = makeSentence(words)
+
+const words = ["My", "name", "is", "John", "!"];
+
+function makeSentence(arr) {
+  return arr.join(" ");
+}
+
+const sentence = makeSentence(words);
+console.log(sentence);
+
+// Задание 8: "Количество элементов"
+// Создай массив `items`
+// Создай функцию `getCount(arr)`:
+// - возвращает количество элементов в массиве
+// Сохрани результат в переменную и выведи в консоль
+// Пример:
+// const count = getCount(items)
+
+const items = ["apple", "chery", "lemon"];
+
+function getCount(arr) {
+  return arr.length;
+}
+
+const count = getCount(items);
+console.log(count);
+
+// Задание 9: "Уникальность элемента"
+// Создай массив `emails`
+// Создай функцию `addEmail(arr, email)`:
+// - если email уже есть — не добавляет
+// - если нет — добавляет в массив
+// - возвращает массив
+// Сохрани результат в переменную и выведи в консоль
+// Пример:
+// const updatedEmails = addEmail(emails, "test@mail.com")
+
+const emails = ["test@mail.com"];
+
+function addEmail(arr, email) {
+  if (!arr.includes(email)) {
+    arr.push(email);
+    return arr;
   }
+  return arr;
 }
 
-
-console.log(fullOrder)
-
-fullOrder.buyer.name = "Vasiliy"
-fullOrder.buyer.email = "vasya@gmail.com"
-fullOrder.buyer.phone = "1111-2222"
-
-fullOrder.shipping.city = "Toronto"
-fullOrder.shipping.adress = "Waterfall Str 6"
-fullOrder.shipping.shipMethod = "truck"
-
-fullOrder.payment.status = "delivered"
-fullOrder.payment.paymentMethod = "bank"
-
-console.log(fullOrder)
-
-// Задание 6: "Метод приветствия"
-// Создай объект `person`, у которого будут свойства:
-// - `name`
-// - `age`
-// Добавь метод `greet(message)`:
-// - принимает строку message
-// - выводит в консоль: "<message>, меня зовут <name>"
-
-const person = {
-  name : "John",
-  age : 19 ,
-
-greet(message) {
-  console.log(`${message}, меня зовут ${this.name}`)
-}
-}
-
-person.greet("Привет")
-
- 
-// Задание 7: "Подсчёт стоимости корзины"
-// Создай объект `cart`, у которого:
-// - есть свойство `items` (массив чисел — цены товаров)
-// Добавь метод `getTotal(discount)`:
-// - принимает число discount (скидка в процентах)
-// - считает сумму всех товаров
-// - применяет скидку
-// - возвращает итоговую сумму
-
-const cart = {
-  items: [40, 30, 65, 94],
-
-  getTotal(discount) {
-    let total = 0;
-    
-    for (const price of this.items) {
-     total += price 
-    }
-    let finalPrice = total - total * discount / 100;
-    return finalPrice
-}
-}
-console.log (cart.getTotal(50))
-
-
-// Задание 8: "Обновление данных пользователя"
-// Создай объект `profile`, у которого:
-// - `username`
-// - `email`
-// Добавь метод `update(field, value)`:
-// - принимает название поля и новое значение
-// - изменяет соответствующее поле объекта
-// - выведи объект в консоль до и после изменения
-
-const profile = {
-  username: "Agent007",
-  email: "agent007@gmail.com",
-
-  update(field, value) {
-  this[field] = value
-  }
-}
-
-console.log(profile);
-
-profile.update("username","Agent008")
-
-console.log(profile)
+const updatedEmails = addEmail(emails, "test@mail.com");
+console.log(updatedEmails);
