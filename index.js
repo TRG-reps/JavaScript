21b1470b92b0e9aa2674b91a90fb225c38a1fe06
Ruslan <tomash.ocenka@gmail.com>
2026-07-13 18:39:57 +0300

    Complete task-js-6

--- index.js
+++ index.js
@@ -1,166 +1,150 @@
-// Задание 1: "Добавление элемента"
-// Создай массив `fruits`
-// Создай функцию `addFruit(arr, fruit)`:
-// - добавляет fruit в конец массива
-// - возвращает обновлённый массив
-// Сохрани результат вызова функции в переменную и выведи её в консоль
-// Пример:
-// const result = addFruit(fruits, "apple")
 
-const fruits = [];
+// Задача 1 - С помощью метода массива выведи в консоль значение каждого элемента 
+const names = ['Alice', 'Bob', 'Charlie'];
 
-function addFruit(arr, fruit) {
-  arr.push(fruit);
-  return arr;
-}
+names.forEach((name) => console.log(name))
 
-const result = addFruit(fruits, "apple");
-console.log(result);
 
-// Задание 2: "Удаление последнего"
-// Создай массив `numbers`
-// Создай функцию `removeLast(arr)`:
-// - удаляет последний элемент
-// - возвращает удалённое значение
-// Сохрани результат в переменную и выведи в консоль
-// Пример:
-// const removed = removeLast(numbers)
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-function removeLast(arr) {
-  return arr.pop();
-}
 
-const removed = removeLast(numbers);
-console.log(numbers);
+// Задача 2 - Выведи в консоль имена с порядковым номером (начиная с 1):
 
-// Задание 3: "Проверка наличия"
-// Создай массив `users`
-// Создай функцию `hasUser(arr, name)`:
-// - проверяет, есть ли name в массиве
-// - возвращает true или false
-// Сохрани результат в переменную и выведи в консоль
-// Пример:
-// const exists = hasUser(users, "Alex")
+// Результат:
+// 1. Alice  
+// 2. Bob  
+// 3. Charlie
 
-const users = [];
 
-function hasUser(arr, name) {
-  return arr.includes(name);
-}
 
-const exists = hasUser(users, "Alex");
-console.log(exists);
+const names = ['Alice', 'Bob', 'Charlie']
 
-// Задание 4: "Получить индекс"
-// Создай массив `colors`
-// Создай функцию `getIndex(arr, color)`:
-// - возвращает индекс элемента
-// - если нет — возвращает -1
-// Сохрани результат в переменную и выведи в консоль
-// Пример:
-// const index = getIndex(colors, "red")
+names.forEach((element, index) => console.log(`${index + 1}. ${element}`))
 
-const colors = ["red"];
 
-function getIndex(arr, color) {
-  return arr.indexOf(color);
-}
 
-const index = getIndex(colors, "red");
-console.log(index);
+// Задача 3 - У тебя массив заказов orders
+const orders = [
+  { id: 101, product: 'Laptop' },
+  { id: 102, product: 'Phone' },
+  { id: 103, product: 'Tablet' }
+];
 
-// Задание 5: "Добавление в начало"
-// Создай массив `tasks`
-// Создай функцию `addTaskToStart(arr, task)`:
-// - добавляет задачу в начало массива
-// - возвращает массив
-// Сохрани результат в переменную и выведи в консоль
-// Пример:
-// const updatedTasks = addTaskToStart(tasks, "Сделать ДЗ")
+// Используя метод массива, выведи в консоль сообщение для каждого заказа: 
+// Заказ #101: Laptop
+// Заказ #102: Phone
+// ...
 
-const tasks = [];
 
-function addTaskToStart(arr, task) {
-  arr.unshift(task);
-  return arr;
-}
 
-const updatedTasks = addTaskToStart(tasks, "Сделать ДЗ");
-console.log(updatedTasks);
+const orders = [
+  { id: 101, product: 'Laptop' },
+  { id: 102, product: 'Phone' },
+  { id: 103, product: 'Tablet' }
+]
 
-// Задание 6: "Удаление первого элемента"
-// Создай массив `messages`
-// Создай функцию `removeFirst(arr)`:
-// - удаляет первый элемент массива
-// - возвращает его
-// Сохрани результат в переменную и выведи в консоль
-// Пример:
-// const first = removeFirst(messages)
+orders.forEach((order) => console.log(`Заказ ${order.id}: ${order.product}`))
 
-const messages = ["Hi", "Hello"];
 
-function removeFirst(arr) {
-  return arr.shift();
-}
 
-const first = removeFirst(messages);
-console.log(first);
+// Задача 4 - Удвой все числа в массиве:
+const nums = [1, 2, 3];
+// Верни новый массив: [2, 4, 6]
 
-// Задание 7: "Преобразование в строку"
-// Создай массив `words`
-// Создай функцию `makeSentence(arr)`:
-// - объединяет элементы массива в строку через пробел
-// - возвращает строку
-// Сохрани результат в переменную и выведи в консоль
-// Пример:
-// const sentence = makeSentence(words)
 
-const words = ["My", "name", "is", "John", "!"];
 
-function makeSentence(arr) {
-  return arr.join(" ");
-}
 
-const sentence = makeSentence(words);
-console.log(sentence);
 
-// Задание 8: "Количество элементов"
-// Создай массив `items`
-// Создай функцию `getCount(arr)`:
-// - возвращает количество элементов в массиве
-// Сохрани результат в переменную и выведи в консоль
-// Пример:
-// const count = getCount(items)
+const nums = [1, 2, 3]
 
-const items = ["apple", "chery", "lemon"];
+const double = nums.map((numb) => numb * 2)
+console.log(double)
 
-function getCount(arr) {
-  return arr.length;
-}
 
-const count = getCount(items);
-console.log(count);
 
-// Задание 9: "Уникальность элемента"
-// Создай массив `emails`
-// Создай функцию `addEmail(arr, email)`:
-// - если email уже есть — не добавляет
-// - если нет — добавляет в массив
-// - возвращает массив
-// Сохрани результат в переменную и выведи в консоль
-// Пример:
-// const updatedEmails = addEmail(emails, "test@mail.com")
+// Задача 5 - Из массива имён создай массив объектов:
+const arrayNames = ['Alice', 'Bob'];
 
-const emails = ["test@mail.com"];
+//  Нужно получить:
+[
+  { name: 'Alice', length: 5 },
+  { name: 'Bob', length: 3 }
+]
 
-function addEmail(arr, email) {
-  if (!arr.includes(email)) {
-    arr.push(email);
-    return arr;
-  }
-  return arr;
-}
 
-const updatedEmails = addEmail(emails, "test@mail.com");
-console.log(updatedEmails);
+
+const arrayNames = ['Alice', 'Bob']
+
+ const objNames = arrayNames.map((arrName) => ({ name: arrName, length: arrName.length})) 
+ console.log(objNames)
+
+
+
+// Задача 6 - У тебя массив пользователей users:
+const users = [
+  { name: 'Alice', age: 20 },
+  { name: 'Bob', age: 17 }
+];
+
+// Создай новый массив строк вида: ['Alice (20 лет)', 'Bob (17 лет)']
+
+
+
+const users = [
+  { name: 'Alice', age: 20 },
+  { name: 'Bob', age: 17 }
+]
+
+const newUsers = users.map((user) => `${user.name} (${user.age} лет)`)
+console.log(newUsers)
+
+
+
+// Задача 7 - Отфильтруй пользователей старше 18 лет:
+const dataUsers = [
+  { name: 'Alice', age: 20 },
+  { name: 'Bob', age: 17 },
+  { name: 'Charlie', age: 25 }
+];
+//  Ожидается только те, кто age > 18
+
+
+
+const dataUsers = [
+  { name: 'Alice', age: 20 },
+  { name: 'Bob', age: 17 },
+  { name: 'Charlie', age: 25 }
+];
+
+console.log(dataUsers.filter((user) => user.age > 18))
+
+
+
+// Задача 8 - У тебя массив заказов, нужно оставить только те, где статус — 'delivered':
+const dataOrders = [
+  { id: 1, status: 'delivered' },
+  { id: 2, status: 'pending' },
+  { id: 3, status: 'delivered' }
+];
+
+//  Результат в консоли:
+[
+  { id: 1, status: 'delivered' },
+  { id: 3, status: 'delivered' }
+
+  
+
+  const dataOrders = [
+    { id: 1, status: 'delivered' },
+    { id: 2, status: 'pending' },
+    { id: 3, status: 'delivered' }
+  ]
+
+
+  
+  console.log(dataOrders.filter((order) => order.status === 'delivered'))
+
+
+
+
+
+
